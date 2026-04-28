abb3dd5e6b56c03f1d46cb06f852fd8c7446e946
StormM1xed <niemirmikolaj@gmail.com>
2026-04-28 13:47:25 +0200

    Add files via upload

--- JavaScript.js
+++ JavaScript.js
@@ -1,39 +1,26 @@
 function Even() {
-    let num = document.querySelector("#num").value
+    let num = prompt("Podaj liczbę")
     if (num % 2 == 0) {
-        document.write("Twoja liczba jest parzysta.")
+        console.log("Twoja liczba jest parzysta.")
     }else{
-        document.write("Twoja liczba jest nieparzysta.")
+        console.log("Twoja liczba jest nieparzysta.")
     }
     
 }
 //Even()
 
 function Delta() {
-    let a = document.querySelector("#A").value
-    let b = document.querySelector("#B").value
-    let c = document.querySelector("#C").value
+    let a = prompt("Podaj liczę A")
+    let b = prompt("Podaj liczę B")
+    let c = prompt("Podaj liczę C")
 
     let delta = (b * b) - (4 * a * c)
-    document.write("Delta wynosi " + delta + "<br>")
-    b = b - (b * 2)
-    if (delta < 0) {
-        document.write("Nie ma pierwiastków")
-    } else if (delta = 0) {
-        let x0 = -b / (2 * a)
-        document.write("Pierwiastek X-0 to " + x0)
-    } else {
-        let x1 = -b - Math.sqrt(delta) / (2 * a)
-        let x2 = -b + Math.sqrt(delta) / (2 * a)
-        document.write("Pierwiastek X-1 to " + x1 + "<br>")
-        document.write("Pierwiastek X-2 to " + x2)
-    }
+    console.log("Delta wynosi " + delta)
 }
 //Delta()
 
-function Join() {
-    let inp1 = document.querySelector("#i1").value
-    let inp2 = document.querySelector("#i2").value
-    output = inp1 + inp2
-    document.write(output)
-}
+let but = document.getElementById("b1")
+    but.addEventListener('click', function(){
+    let inp1 = document.getElementById("i1")
+    let inp2 = document.getElementById("i2")
+})
